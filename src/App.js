36dc896b9ee763ad23a7cc3f15399d9e0ce515f7
AmeenaCpt <shaikashikamina@gmail.com>
2025-07-25 10:45:18 +0530

    Added Counter component and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-function Greeting() {
-  const name = "Ameena";
-  return <h2>Hello, {name}! Welcome to JSX.</h2>;
-}
-
-export default function App() {
+import Counter from './components/Counter';
+export default function App(){
   return (
     <div>
-      <Greeting />
+      <Counter/>  
     </div>
   );
 }
-
